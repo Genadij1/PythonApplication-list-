e6f9f48d93605f41d452d69d8de485342e44a4fa
Genacud <bgennadij265@icloud.com>
2023-04-01 22:41:48 +0300

    updata2

--- PythonApplication_list_.py
+++ PythonApplication_list_.py
@@ -18,18 +18,9 @@ for i in list :
     if i%2 != 0:
         n += i
 print("sum unpaired", n)
-prod = 1
-for i in list:
-    if i%3 == 0:
-        prod *= i
-print("prod", prod)
-prod_min_max=1
-min_num = min(list)
-max_num = max(list)
-for i in list:
-    if i > min_num and i < max_num:
-        prod_min_max *= i
-print("prod_min_max", prod_min_max)
+print(list[index])
+
+
 
 
 
