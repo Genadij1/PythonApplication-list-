d17d48650f22c036fc3344f134b11d494e926ab2
Genacud <bgennadij265@icloud.com>
2023-04-04 22:29:49 +0300

    ex_2-done

--- PythonApplication_list_.py
+++ PythonApplication_list_.py
@@ -1,1 +1,28 @@
 
+import random
+
+list = [random.randint(-10,10) for i in range(12)]
+print(list)
+paired=0
+for i in list:
+    if i>0 and i%2==0:
+        paired=i
+        print('paired', paired)
+print()
+odd=0
+for i in list:
+    if i>0 and i%2 != 0:
+        odd=i
+        print('odd', odd)
+print()
+neg=0
+for i in list:
+    if i<0:
+        neg=i
+        print('neg', neg)
+print()
+pos=0
+for i in list:
+    if i>0:
+        pos=i
+        print('pos', pos)
