fa227048c8d55c98effd0d2e3ae8e468d07c54c5
Genacud <bgennadij265@icloud.com>
2023-04-04 22:51:07 +0300

    update

--- PythonApplication_list_.py
+++ PythonApplication_list_.py
@@ -1,1 +1,18 @@
 
+import random
+from re import I
+
+list = [random.randint(-20,20) for i in range (12)]
+print(list)
+par=0
+for i in list:
+    if i%2==0:
+        par+=i
+        print('par', par)
+print()
+odd=0
+for i in list:
+    if i%2!=0:
+        odd+=i
+        print('odd', odd)
+
