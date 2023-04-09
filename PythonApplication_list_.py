ba08b3025ed9eacfc1a9993ad103a1def4deabfa
Genacud <bgennadij265@icloud.com>
2023-04-09 18:14:02 +0300

    ex_1-done

--- PythonApplication_list_.py
+++ PythonApplication_list_.py
@@ -1,18 +1,18 @@
 
 import random
 list = []
-for i in range (13):
+for i in range (12):
     list.append(random.randint(-10, 10))
 print(list)
 par=0
 for i in list:
     if i%2==0:
-        par+=i
+        par+=1
 print('par', par)
 
 odd=0
 for i in list:
     if i%2!=0:
-        odd+=i
+        odd+=1
 print('odd', odd)
 
