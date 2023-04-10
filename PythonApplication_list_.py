64af55c1e9c842eeeee6ac2b96ced1518c7db878
Genacud <bgennadij265@icloud.com>
2023-04-10 22:50:01 +0300

    ex_2-done

--- PythonApplication_list_.py
+++ PythonApplication_list_.py
@@ -1,25 +1,20 @@
-
 import random
-random_list = [random.randint(-10, 10) for i in range(10)]
-
-even_numbers = []
-odd_numbers = []
-negative_numbers = []
-positive_numbers = []
-#func
-for number in random_list:
-    if number % 2 == 0:
-        even_numbers.append(number)
-    else:
-        odd_numbers.append(number)
-    if number < 0:
-        negative_numbers.append(number)
-    elif number > 0:
-        positive_numbers.append(number)
-
-
-print("Випадковий список:", random_list)
-print("Парні числа:", even_numbers)
-print("Непарні числа:", odd_numbers)
-print("Від'ємні числа:", negative_numbers)
-print("Додатні числа:", positive_numbers)
+list = []
+for i in range (13):
+    list.append(random.randint(-10, 10))
+print(list)
+for i in list:
+    if i%2==0:
+        print('paired', i)
+print()
+for i in list:
+    if i>0 and i%2 != 0:
+        print('odd', i)
+print()
+for i in list:
+    if i<0:
+        print('neg', i)
+print()
+for i in list:
+    if i>0:
+        print('pos', i)
