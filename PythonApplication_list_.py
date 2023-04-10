cde8b634b0f8b63db0020cbedfb03ae7cf591a66
Genacud <bgennadij265@icloud.com>
2023-04-10 22:58:25 +0300

    ex_9-done

--- PythonApplication_list_.py
+++ PythonApplication_list_.py
@@ -1,1 +1,12 @@
 
+nums = input("Введіть числа через пробіл: ").split()
+nums = [int(num) for num in nums]
+
+average = sum(nums) / len(nums)
+
+count = 0
+for num in nums:
+    if num > average:
+        count += 1
+
+print("Кількість чисел, більших за середнє арифметичне:", count)
