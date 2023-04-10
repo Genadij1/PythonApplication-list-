8e5b7327e6873c55305781eed820a21f19d7a588
Genacud <bgennadij265@icloud.com>
2023-04-10 22:56:09 +0300

    ex_6-done

--- PythonApplication_list_.py
+++ PythonApplication_list_.py
@@ -1,1 +1,9 @@
 
+nums = input("Введіть числа через пробіл: ").split()
+nums = [int(num) for num in nums]
+
+squares = []
+for num in nums:
+    squares.append(num**2)
+
+print("Квадрати чисел у списку:", squares)
