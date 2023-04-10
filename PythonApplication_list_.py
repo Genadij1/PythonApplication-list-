87794470d32f47aa740cf831c11f911ac0f23c93
Genacud <bgennadij265@icloud.com>
2023-04-10 22:56:56 +0300

    ex_7-done

--- PythonApplication_list_.py
+++ PythonApplication_list_.py
@@ -1,1 +1,11 @@
 
+nums = input("Введіть числа через пробіл: ").split()
+nums = [int(num) for num in nums]
+
+min_index = nums.index(min(nums))
+max_index = nums.index(max(nums))
+
+nums[min_index], nums[max_index] = nums[max_index], nums[min_index]
+
+print("Список зі зміненими місцями мінімального та максимального елементів:")
+print(nums) 
