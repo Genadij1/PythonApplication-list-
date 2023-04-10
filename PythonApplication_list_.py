ef4089df630dd9dd54dfe3ca6b38aa33bb16bda9
Genacud <bgennadij265@icloud.com>
2023-04-10 22:55:19 +0300

    ex_5-done

--- PythonApplication_list_.py
+++ PythonApplication_list_.py
@@ -1,1 +1,10 @@
 
+nums = input("Введіть числа через пробіл: ").split()
+nums = [int(num) for num in nums]
+
+positive_nums = []
+for num in nums:
+    if num > 0:
+        positive_nums.append(num)
+
+print("Додатні числа у списку:", positive_nums)
