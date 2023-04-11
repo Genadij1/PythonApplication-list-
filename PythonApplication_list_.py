94794555ea475f4fe81ae3f3bb562465f13e3bf4
Genacud <bgennadij265@icloud.com>
2023-04-11 16:14:59 +0300

    не разобрался

--- PythonApplication_list_.py
+++ PythonApplication_list_.py
@@ -1,7 +1,1 @@
 
-def common_elements(list1, list2):
-    common = []
-    for element in list1:
-        if element in list2 and element not in common:
-            common.append(element)
-    return common
